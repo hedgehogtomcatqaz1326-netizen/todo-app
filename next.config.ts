d23c11f22d43b01e52eb3c225d53d3deccbd865c
okuno <hedgehogtomcat.qaz1326@gmail.com>
2026-08-14 22:18:30 +0900

    Disable eslint check on build in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+} as unknown as NextConfig;
 
 export default nextConfig;
